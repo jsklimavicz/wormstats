229b5ecfdab9a339e275db2d4a18eace83639ad4
jsklimavicz <42780890+jsklimavicz@users.noreply.github.com>
2021-09-09 09:54:48 -0400

    Update compound.py

--- compound.py
+++ compound.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 class Compound:
 	def __init__(self, **kwargs):
 		self.info = {"name": None,
-				"id": None,
+				"ids": None,
+				"test_dates": None,
 				"max_conc": None,
 				"n_trials": None, 
 				"conc": None,
@@ -14,15 +15,23 @@ class Compound:
 				"dead_count": None,
 				"plate_ids": None}
 		for k, v in kwargs.items(): self.info[k] = v
+		self.curve_data = None
+		self.data = None
 	def fit_data(self, **kwargs):
 		for k, v in kwargs.items(): self.info[k] = v
 		self.curve_data = CI_finder(**self.info)
 	def get_EC_CIs(self, EC = np.array([0.1, 0.5, 0.9]), CI_val=0.95, options=None):
-		return self.curve_data.get_EC_CIs(EC = np.array([0.1, 0.5, 0.9]), CI_val=0.95, options=None)
+		if self.curve_data is None: self.fit_data()
+		return self.curve_data.get_EC_CIs(EC = EC, CI_val=CI_val, options=options)
+	def get_plot(self):
+		self.curve_data.plot_CIs()
 
 
 
-
-ch = Compound(name="malathion", id="1036-14A", conc=conc, live_count=live_count, plate_ids=plate_ids, dead_count=dead_count)
-ch.fit_data()
-ch.get_EC_CIs()
+# plate_ids=[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+# conc = np.array([-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7])
+# live_count = np.array([11, 11, 12, 3, 3, 2, 0, 0, 0, 0, 9, 8, 7, 7, 5, 2, 1, 1, 0, 0, 11, 12, 14, 3, 5, 1, 0, 1, 0, 0, 9, 11, 5, 9, 3, 1, 0, 0, 0, 0])
+# dead_count = np.array([5, 5, 8, 17, 14, 15, 14, 15, 18, 15, 5, 6, 5, 9, 10, 14, 18, 17, 19, 13, 4, 4, 6, 12, 10, 14, 13, 12, 19, 19, 4, 3, 10, 6, 11, 18, 14, 15, 16, 11])
+# ch = Compound(name="malathion", id="1036-14A", conc=conc, live_count=live_count, plate_ids=plate_ids, dead_count=dead_count)
+# ch.fit_data()
+# ch.get_EC_CIs()
