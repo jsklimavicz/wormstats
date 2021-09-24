a3c31c85bd59ea69d75e3656bf38c07da303ff64
jsklimavicz <42780890+jsklimavicz@users.noreply.github.com>
2021-09-24 15:01:06 -0400

    Update latex_writer.py

--- latex_writer.py
+++ latex_writer.py
@@ -14,41 +14,35 @@ import time
 
 
 class Latex_Writer:
-	gridx = 4
-	gridy = 6
+	gridx = 2
+	gridy = 4
 	bs = '\\'
 
-	def __init__(self, filename, data, args, title = "Chemical Structures of Ligand Binding Compounds"):
-		self.verbose = args.verbose
-		self.image_folder = args.imagepath
-		self.output = args.output + ".tex"
-		self.df = data
+	def __init__(self, title = "Merlin Bioassay Results"):
 		self.page_max = args.p
 		self.make_header()
 		self.make_title(title)
-		self.make_header()
-		self.section_count = 0
-		self.images_made = 0
-		self.section_bodies = []
-		self.section_driver()
-		self.make_end()
-		self.write_file()
+		self.cmpd_graph_list = []
+		self.graph_count = 0
+		# self.section_driver()
+		# self.make_end()
+		# self.write_file()
 
 
 	def make_header(self):
 		self.header = ["\\documentclass{article}"]
-		self.header.append("\\usepackage[utf8]{inputenc}")
+		# self.header.append("\\usepackage[utf8]{inputenc}")
 		self.header.append("\\usepackage[letterpaper,left=0.75in, right=0.75in, top=0.75in, bottom=0.75in]{geometry}")
 		#Packages for imagesPackages for images
 		self.header.append("\\usepackage{graphicx}")
-		self.header.append("\\usepackage[dvipsnames]{xcolor}")
-		self.header.append("\\definecolor{TTgreen}{cmyk}{0.56, 0, 82, 0.26}")
+		# self.header.append("\\usepackage[dvipsnames]{xcolor}")
+		# self.header.append("\\definecolor{TTgreen}{cmyk}{0.56, 0, 82, 0.26}")
 		# self.header.append("\\usepackage{svg}")
 		self.header.append("\\usepackage[font=scriptsize]{subcaption}")
 		self.header.append("\\captionsetup[subfigure]{format=hang,justification=raggedright,singlelinecheck=false}")
 		#Packages for symbols and chemistry text
 		self.header.append("\\usepackage{amsmath,amsthm,amsfonts,amssymb,mathtools}")
-		self.header.append("\\usepackage[version=4]{mhchem}")
+		# self.header.append("\\usepackage[version=4]{mhchem}")
 		self.header.append(f"\\graphicspath{{{{{self.image_folder:s}}}}}")
 		self.header.append("\\setcounter{topnumber}{8}")
 		self.header.append("\\setcounter{bottomnumber}{8}")
@@ -78,109 +72,96 @@ class Latex_Writer:
 		self.end = ["\n\n"]
 		self.end.append("\\end{document}")
 
+	def make_cmpd_caption(self, name, lc50, lc50CI, R2, reps):
+		'''
+		<Compound Name>
+		LC50: <LC50> <CI>; Curve r^2: <R2> 
+		Biological Replicates: <Bio reps>
+		'''
+		caption = f'{name} \\\\'
+		caption += 'LC_{50} ' + f'{lc50} ppm {lc50CI} \\\\'
+		if reps == 1:
+			caption += f'{reps} biologicial replicate; R^2: {r2}'
+		else:
+			caption += f'{reps} biologicial replicates; R^2: {r2}'
+		#TODO: add reference compound rel. potency
+		return caption
 
-	def zinc_png_checker(self, zincID, smiles):
-		mod_id = "_".join(zincID.split("."))
-		image_name = self.image_folder + "/" + mod_id 
-		if not os.path.exists(image_name+".eps"): 
-			self.images_made += 1
-			if self.verbose and (self.images_made % 10 == 0) : print(f"Making image {self.images_made:n}")
-			os.system(f'obabel -:"{smiles:s}" -O {image_name:s}.svg -xp 144  -xC -xs > /dev/null 2>&1')
-			with open(image_name+".svg", 'r') as file_in:
-				with open("temp.svg", 'w') as file_out:
-					for line in file_in:
-						line = line.replace('width="144px" height="144px" viewBox="0 0 100 100">', 'width="144px" height="96px" viewBox="0 0 144 96">')
-						line = line.replace('width="100" height="100"', 'width="144px" height="96px"')
-						file_out.write(line)
-			os.system(f'cp temp.svg {image_name:s}.svg')
-			os.system(f'inkscape {image_name:s}.svg -E {image_name:s}.eps > /dev/null 2>&1')
-		return image_name + ".eps"
+	def make_cmpd_graph(self, img_path, name, lc50, lc50CI, R2, reps):
+		graph_lines = []
+		img_line = f"\\includegraphics[width = {{0.95\\textwidth}}]{{{img_path:s}}}"
+		name, alpha_name = latexify_name(name)
+		caption = make_cmpd_caption(name, lc50, lc50CI, R2, reps)
+		graph_lines.append(f"   \\begin{{subfigure}}{{{image_width:.3f}\\textwidth}}")
+		graph_lines.append("      \\centering")
+		graph_lines.append("      "+img_line)
+		graph_lines.append("      \\vspace{-0.35cm}")
+		graph_lines.append("      "+caption)
+		graph_lines.append("   \\end{subfigure}%")
+		self.graph_count += 1
+		self.cmpd_graph_list.append(alpha_name, graph_lines) 
 
-	def mod_zinc_id(self, zincID):
-		id = zincID.split(".")[0]
-		res = re.split('[ZINC|JSK]+[0]+', id)
-		if "JSK" in id: 
-			return "JSK" + res[1]
-		else: 
-			return "ZINC" + res[1]
-		
 
-	def make_section(self, section_dict):
-		section = [f"\\section*{{Group {self.section_count + 1:n}}}"]
-		section.append("\\vspace{-0.7cm}")
+	def make_body(self):
+		self.body = []
 		img_count = 0
 		pages = 0
 		row_end = False
 		image_width = 1/self.gridx
-		for index, (row, row_dict) in enumerate(section_dict.items()):
-			smiles = row_dict["SMILES"]
-			zincID = self.mod_zinc_id(row_dict["ZincID"])
-			image_name = self.zinc_png_checker(zincID, smiles)
-			zincID = self.mod_zinc_id(row_dict["ZincID"])
-			if "JSK" in zincID:
-				caption = f'\\caption*{{\\textcolor{{TTgreen}}{{TT-{zincID:s}}} '
-			else: 
-				caption = f'\\caption*{{{zincID:s} '
-			caption += f'E: {float(row_dict["Energy"]):.1f}, F: {float(row_dict["Efficiency"]):.3f}\\\\ \\ce{{{row_dict["Formula"]:s}}}, MW: {float(row_dict["MW"]):.2f} \\\\ {row_dict["Time"]:s}}}'
-			line = f"\\includegraphics[width = {{0.95\\textwidth}}]{{{image_name:s}}}"
-			if img_count % (self.gridx * self.gridy) == 0:  section.append("\\begin{figure}[thp!]")
-			section.append(f"   \\begin{{subfigure}}{{{image_width:.3f}\\textwidth}}")
-			section.append("      \\centering")
-			section.append("      "+line)
-			section.append("      \\vspace{-0.35cm}")
-			section.append("      "+caption)
-			section.append("   \\end{subfigure}%")
+		self.cmpd_graph_list = self.cmpd_graph_list.sort(key=lambda x:x[0])
+		for index, (alpha_name, graph_lines) in enumerate(self.cmpd_graph_list.items()):
+			if img_count % (self.gridx * self.gridy) == 0:  self.body.append("\\begin{figure}[thp!]")
+			self.body.append(i) for i in graph_lines
+			self.body.append("\n")
 			img_count += 1
 			if img_count % (self.gridx * self.gridy) == 0: 
-				section.append("\\end{figure}")
+				self.body.append("\\end{figure}")
 				img_count = 0
 				pages += 1
 				if pages >= self.page_max:
 					break
-				section.append("\\clearpage")
-				section.append("\\pagebreak")
-			if img_count % self.gridx == 0: section.append("\\vspace{-0.1cm}")
+				self.body.append("\\clearpage")
+				self.body.append("\\pagebreak")
+			if img_count % self.gridx == 0: self.body.append("\\vspace{-0.1cm}")
 
 		if img_count % (self.gridx * self.gridy) != 0:
-			section.append("\\end{figure}")
-		section.append("\\pagebreak")
-		section.append("\n\n")
-		self.section_bodies.append(section)
-		self.section_count += 1	
+			self.body.append("\\end{figure}")
+		self.body.append("\\pagebreak")
+		self.body.append("\n\n")
+
 
 
 	def write_file(self):
 		file = open(self.output, 'w')
 		file.write("\n".join(self.header))
 		file.write("\n".join(self.title))
-		for section in self.section_bodies:
-			file.write("\n".join(section))
+		file.write("\n".join(self.body))
 		file.write("\n".join(self.end))
 		file.close()
 
 
-	def section_driver(self):
-		unique_groups = self.df.group.unique()
-		for group in unique_groups:
-			# "ZincID,Time,ReceptorID,nHeavyAtoms,Model,Energy,Efficiency,Formula,MW,nRings,logP,PSA,MR,SMILES\n"
-			df = self.df.loc[self.df['group'] == group]
-			df = df[["ZincID", "Time", "Energy", "Efficiency", "Formula", "MW", "logP", "SMILES"]]
-			section_dict = df.to_dict('index')
-			if self.verbose: print(f"On section {self.section_count+1:n}")
-			self.make_section(section_dict)
-			# if self.section_count >= 1 : break #for debug
-
-
-
-if __name__ == "__main__":
-
-	df=pd.read_csv(args.grouped_data, sep=',')
-	# print(args)
-	writer = Latex_Writer(filename = "temp.tex", data = df, args = args)
-	if args.verbose: print("Writing pdf file...")
-	os.system(f'pdflatex --shell-escape {args.output:s}.tex > pdf_tex.log 2>&1')
-	if args.verbose: print("Done!")
-
-
-
-
+def latexify_name(name):
+	greek_letters = ['alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta', 'eta', 'lambda']
+	mod = False
+	alpha_name = name
+	if name[:2].lower() == "s-": 
+		alpha_name = name[2:]
+		name = "\\textit{S}-" + alpha_name
+		mod = True
+	elif name[:2].lower() == "r-": 
+		alpha_name = name[2:]
+		name = "\\textit{R}-" + alpha_name
+		mod = True
+	else:
+		for letter in greek_letters:
+			if name[:len(letter)+1].lower() = letter+'-': 
+				alpha_name = name[len(letter):]
+				name = "\\" + letter + alpha_name
+				mod = True
+				break
+	if len(name)< 5:
+		return name.upper(), alpha_name.lower()
+	elif mod:
+		return name, alpha_name.lower()
+	else:
+		return name[0].capitalize() + name[1:], alpha_name.lower()
